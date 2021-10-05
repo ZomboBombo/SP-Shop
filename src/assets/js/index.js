abd8b4384be8924cc9432b81a2665075029a16cc
Sergey Novikov <foxinazombobombo@mail.ru>
2021-10-05 12:14:08 +0300

    Initialize calls of main functions
    In index.js calls of main functions have been initialized.

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,31 +1,10 @@
-import { gsap } from 'gsap';
+import activateBasketButton from './basketButton';
+import productCardLogic from './productCard';
+import totalCostCalculation from './totalCost';
 
-// import { ScrollToPlugin } from 'gsap/ScrollToPlugin.js';
-// gsap.registerPlugin(ScrollToPlugin);
-
-global.gsap = gsap;
-
-gsap.defaults({
-	overwrite: 'auto',
-});
-
-class ProjectApp {
-	constructor() {
-		this.env = require('./utils/env').default;
-		this.utils = require('./utils/utils').default;
-		this.classes = {
-			Signal: require('./classes/Signal').default,
-		};
-		this.components = {};
-		this.helpers = {};
-		this.modules = {};
-		document.addEventListener('DOMContentLoaded', () => {
-			document.documentElement.classList.remove('_loading');
-		});
-	}
-}
-
-global.ProjectApp = new ProjectApp();
+activateBasketButton();
+productCardLogic();
+totalCostCalculation();
 
 if (module.hot) {
 	module.hot.accept();
