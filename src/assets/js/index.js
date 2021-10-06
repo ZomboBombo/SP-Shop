23eae4e5cdd9edb797b45571395975b00f5f82f3
Sergey Novikov <foxinazombobombo@mail.ru>
2021-10-07 00:58:35 +0300

    Add calls of new functions

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,10 +1,14 @@
 import activateBasketButton from './basketButton';
 import productCardLogic from './productCard';
 import totalCostCalculation from './totalCost';
+import trackEmptyBasket from './emptyBasketFiller';
+import openNavigationMenu from './openNavigationMenu';
 
 activateBasketButton();
 productCardLogic();
 totalCostCalculation();
+trackEmptyBasket();
+openNavigationMenu();
 
 if (module.hot) {
 	module.hot.accept();
