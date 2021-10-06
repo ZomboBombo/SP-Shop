58d995e9d7ea39e54103530f6135034e907aa4be
Sergey Novikov <foxinazombobombo@mail.ru>
2021-10-07 00:36:00 +0300

    Add empty basket filler
    Added empty basket filler.
    
    Described the logic for displaying an empty basket notification.
    Also, added styles for text-block with notification.

---
+++ src/assets/js/emptyBasketFiller.js
@@ -0,0 +1,40 @@
+import Util from 'utils/utility-constants';
+import getNewObserver from './observerOfElementChanges';
+
+// ---------- КОНСТАНТЫ -----------
+const EMPTY_BASKET_NOTIFICATION_SHOW_MOD = 'empty-basket-notification--show';
+const BASKET_CONTENT_LIST_HIDE_MOD = 'basket-content__products--hide';
+
+// --------- DOM-элементы ---------
+const basketContent = document.querySelector('#basket-content');
+const basketContentProductList = basketContent.querySelector('#basket-content-products-list');
+const emptyBasketNotification = basketContent.querySelector('#empty-basket-notification');
+
+/*
+===============================================
+--------------- ОСНОВНАЯ ЛОГИКА ---------------
+===============================================
+*/
+const notifyAboutemptyBasket = () => {
+	switch (Util.IS_TRUE) {
+		case basketContentProductList.children.length === Util.ZERO_VALUE:
+			emptyBasketNotification.classList.add(EMPTY_BASKET_NOTIFICATION_SHOW_MOD);
+			basketContentProductList.classList.add(BASKET_CONTENT_LIST_HIDE_MOD);
+			break;
+
+		default:
+			emptyBasketNotification.classList.remove(EMPTY_BASKET_NOTIFICATION_SHOW_MOD);
+			basketContentProductList.classList.remove(BASKET_CONTENT_LIST_HIDE_MOD);
+	}
+};
+
+/*
+=============================================================================================
+--------- Экспортируемая функция, описывающая логику отслеживания "пустоты" Корзины ---------
+=============================================================================================
+*/
+const trackEmptyBasket = () => {
+	getNewObserver(basketContentProductList, notifyAboutemptyBasket);
+};
+
+export default trackEmptyBasket;
