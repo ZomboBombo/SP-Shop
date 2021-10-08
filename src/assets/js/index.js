2a712c731d5f47fbd6ef271e2a19670ae3183eef
Sergey Novikov <foxinazombobombo@mail.ru>
2021-10-08 04:16:39 +0300

    Add imports of new modules

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -3,12 +3,14 @@ import productCardLogic from './productCard';
 import totalCostCalculation from './totalCost';
 import trackEmptyBasket from './emptyBasketFiller';
 import openNavigationMenu from './openNavigationMenu';
+import paymentLogic from './paymentLogic';
 
 activateBasketButton();
 productCardLogic();
 totalCostCalculation();
 trackEmptyBasket();
 openNavigationMenu();
+paymentLogic();
 
 if (module.hot) {
 	module.hot.accept();
